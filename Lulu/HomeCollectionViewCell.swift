6bf80259af249e592977feb205e2348ee1d50b76
Scott Campbell <cdn.zhredder@gmail.com>
2016-10-30 16:11:51 -0600

    Home tab has scrollable collection view with reusable cells. First run at models for User and Listing created.

---
+++ Lulu/HomeCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  HomeCollectionViewCell.swift
+//  Lulu
+//
+//  Created by Scott Campbell on 10/30/16.
+//  Copyright © 2016 Team Lulu. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var listingImageView: UIImageView!
+    @IBOutlet weak var listingTitleLabel: UILabel!
+    @IBOutlet weak var listingPriceTag: UIView!
+    @IBOutlet weak var listingPriceLabel: UILabel!
+    
+    // MARK: - Properties
+    var listing: Listing? {
+        didSet {
+            if let list = listing {
+                listingImageView.image = list.photos[0]
+                listingTitleLabel.text = list.title
+                listingPriceTag.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+                listingPriceLabel.text = "$" + String(list.startPrice)
+            }
+        }
+    }
+}
---
+++ Lulu/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  Lulu
+//
+//  Created by Scott Campbell on 10/30/16.
+//  Copyright © 2016 Team Lulu. All rights reserved.
+//
+
+import UIKit
+
+class User {
+
+    // MARK: - Properties
+    var profileImage: UIImage!
+    var firstName: String!
+    var lastName: String!
+    var rating: Int!
+    
+    var postedListings: [Listing]!
+    var soldListings: [Listing]!
+    var favoritedListings: [Listing]!
+    var buyingListings: [Listing]!
+    
+    // User initialization.
+    init(_ profileImage: UIImage, _ firstName: String, _ lastName: String) {
+        self.profileImage = profileImage
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+
