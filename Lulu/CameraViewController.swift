e5a8bcff2abc57d708daa5e4486a1ad190dbccf6
shreyachopra <shreya@shreyas-Device.local>
2016-10-31 23:59:19 -0700

    added file

--- Lulu/CameraViewController.swift
+++ Lulu/CameraViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class CameraViewController: UIViewController {
-
+    
+    //MARK Outlets
+    @IBOutlet weak var addPhotosImage: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var startingPriceTextField: UITextField!
+    @IBOutlet weak var endDateTextField: UITextField!
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var usersNameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var starLabel: UILabel!//static
+    @IBOutlet weak var memberSinceLabel: UILabel!//static
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var repliesInLabel: UILabel!
+    
+    //MARK properties
+    
+    //let tempUserData: User = User(UIImage(named: "duck")!, "Shreya", "Chopra")
+    
+    var user: User? {
+        didSet {
+            if let user = user {
+                usersNameLabel.text = user.firstName + " " + user.lastName
+                userImage.image = user.profileImage
+                ratingLabel.text = String(user.rating) + " Stars"
+                //yearLabel.text = "Member Since " + String(user.memberSince)
+                //repliesInLabel.text = "Replies " + user.replyingHabit
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    /*func loadUserProfile() {
+        
+
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
