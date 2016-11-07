5d5b92e464911a99c31a8908a0aaa8e5e4777875
Jan Clarin <hi@janclarin.com>
2016-11-06 21:15:58 -0700

    Add Facebook Login URL handler setup

--- Lulu/AppDelegate.swift
+++ Lulu/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FacebookCore
 import Firebase
 
 @UIApplicationMain
@@ -16,11 +17,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Configure Facebook SDK.
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         // Initialize Firebase.
         FIRApp.configure()
         return true
     }
-
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        // Configure Facebook SDK.
+        return SDKApplicationDelegate.shared.application(application, open: url, options: options)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
