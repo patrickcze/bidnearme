48eaca7caeabc461729da6cae6f7e751bd4e6e5c
patrickcze <patrick.cze@gmail.com>
2016-11-07 21:03:13 -0700

    Still fixing conflicts

--- Pods/Target Support Files/SZTextView/SZTextView-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SZTextView.h"
-
-FOUNDATION_EXPORT double SZTextViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char SZTextViewVersionString[];
-
