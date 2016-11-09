03a0e97dcb13a44bfc3961fb174698cd4a44f0b9
Patrick <patrick.cze@gmail.com>
2016-11-08 18:00:25 -0700

    Have commented and separated code is tested and is running well

--- Lulu/AppDelegate.swift
+++ Lulu/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Configure Facebook SDK.
         SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
--- Lulu/CameraViewController.swift
+++ Lulu/CameraViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import FirebaseStorage
 import FirebaseDatabase
 
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
-    //MARK Outlets
+class CameraViewController: UIViewController {
+    
+    // MARK: - Outlets
     @IBOutlet weak var addPhotosImage: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descTextArea: UITextView!
@@ -19,52 +20,44 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBOutlet weak var endDateTextField: UITextField!
     @IBOutlet weak var postButtonOutlet: UIButton!
     
-    //MARK properties
+    // MARK: - Properties
     var tempUserData: User!
-    var ref: FIRDatabaseReference!
-    
-    var user: User? {
-        didSet {
-            //            if let user = user {
-            //                usersNameLabel.text = user.firstName + " " + user.lastName
-            //                userImage.image = user.profileImage
-            //                ratingLabel.text = String(user.rating) + " Stars"
-            //                yearLabel.text = "Member Since " + String(user.memberSince)
-            //                repliesInLabel.text = "Replies " + user.replyingHabit
-            //            }
-        }
-    }
+    var firebaseDBReference: FIRDatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ref = FIRDatabase.database().reference()
+        // Setup reference to the database
+        firebaseDBReference = FIRDatabase.database().reference()
         
+        // Establish border colouring and corners on textview and button to matach styles
         descTextArea.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
         descTextArea.layer.borderWidth = 1.0
         descTextArea.layer.cornerRadius = 5.0
-        
-        descTextArea.delegate = self
-        
         postButtonOutlet.layer.cornerRadius = 5.0
         
+        // Setup appropriate delegates
+        descTextArea.delegate = self
         titleTextField.delegate = self
         startingPriceTextField.delegate = self
         endDateTextField.delegate = self
         
+        // Setup toolbar to appear above numebrica keybaord when setting price
         let numberToolbar = UIToolbar()
         numberToolbar.barStyle = UIBarStyle.default
         
-        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(CameraViewController.donePressed))
-        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: #selector(CameraViewController.cancelPressed))
-        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        numberToolbar.setItems([
+            UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: #selector(CameraViewController.cancelPressed)),
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(CameraViewController.donePressed))
+            ], animated: false)
         
-        numberToolbar.setItems([cancelButton, spaceButton, doneButton], animated: false)
         numberToolbar.isUserInteractionEnabled = true
         numberToolbar.sizeToFit()
         
         startingPriceTextField.inputAccessoryView = numberToolbar
         
+        // Setup toolbar to be above keybaord on text area
         let descToolbar = UIToolbar()
         descToolbar.barStyle = UIBarStyle.default
         descToolbar.items = [
@@ -86,10 +79,90 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    //Creates image view ,
+    // Function handles the steps required to take the data on the view and place it into the DB
+    @IBAction func postButtonClicked(_ sender: AnyObject) {
+        print("Post item")
+        
+        // Disable post button while uploading information
+        self.postButtonOutlet.isEnabled = false
+        
+        let listingTitle = titleTextField.text
+        let startPrice = Int(startingPriceTextField.text!)
+        let endDate = endDateTextField.text
+        let desc = descTextArea.text
+        
+        let listingDetails:NSMutableDictionary = [
+            "title": listingTitle ?? "Test",
+            "startPrice": startPrice ?? -1,
+            "endDate": endDate ?? "endDate",
+            "desc": desc ?? "desc",
+            "endDate": " ",
+            "seller":" ",
+            "buyoutPrice": " ",
+            "currentPrice": startPrice ?? -1
+        ]
+        
+        let dbreference = firebaseDBReference.child("listings").childByAutoId()
+        
+        let image = (addPhotosImage.image)!
+        let imageData = UIImageJPEGRepresentation(image, 0.8)
+        
+        uploadImageToFirebase(data: imageData!, listingData: listingDetails, dbreference: dbreference)
+    }
+    
+    // Handles uploading the image to firebase upon which the listing details are sent to the database
+    func uploadImageToFirebase(data: Data, listingData: NSMutableDictionary, dbreference: FIRDatabaseReference) {
+        let dbrefString = String(dbreference.description().characters.suffix(20))
+        
+        let storageRef = FIRStorage.storage().reference(withPath: "listingImages/\(dbrefString).jpg")
+        let uploadMetadata = FIRStorageMetadata()
+        uploadMetadata.contentType = "image/jpeg"
+        
+        var downloadURL:String!
+        print(storageRef)
+        
+        storageRef.put(data as Data, metadata: uploadMetadata) { (metadata, error) in
+            if (error != nil) {
+                // Uh-oh, an error occurred!
+                // TODO: deal with this in some way
+            } else {
+                downloadURL = (metadata!.downloadURL()?.absoluteString)!
+                
+                listingData.addEntries(from: ["imageURL": [downloadURL]])
+                
+                self.uploadListingToDB(listingData, dbreference: dbreference)
+            }
+        }
+    }
+    
+    // Places the listing details in the DB and resets the fields on the page
+    func uploadListingToDB(_ listingDetails: NSMutableDictionary, dbreference: FIRDatabaseReference) {
+        print(listingDetails)
+        
+        dbreference.setValue(listingDetails) { (error, ref) -> Void in
+            if (error != nil) {
+                // Uh-oh, an error occurred!
+                // TODO: deal with this in some way
+            } else {
+                print("Success")
+                
+                self.titleTextField.text = ""
+                self.startingPriceTextField.text = ""
+                self.endDateTextField.text = ""
+                self.descTextArea.text = ""
+                self.addPhotosImage.image = UIImage(named: "addPhotoImage")
+                
+                self.postButtonOutlet.isEnabled = true
+            }
+        }
+    }
+}
+
+extension CameraViewController: UIImagePickerControllerDelegate {
+    //MARK: - UIImagePickerControllerDelegate
+    //Creates image view
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         print("Tapped")
         
@@ -136,19 +209,24 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
         {
             addPhotosImage.image = image
         }
-        else{}
         
         self.dismiss(animated: true, completion: nil)
     }
-    
-    //MARK:UITextFieldDelegate
+}
+
+extension CameraViewController: UINavigationControllerDelegate {
+}
+
+extension CameraViewController: UITextFieldDelegate {
+    //MARK: - UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        //Hide the keybaord
+        //Hide the keyboard
         textField.resignFirstResponder()
         
         return true
@@ -161,89 +239,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     func textFieldDidEndEditing(_ textField: UITextField) {
         
     }
-    
-    //Save data entered
-    @IBAction func postButtonClicked(_ sender: AnyObject) {
-        print("Post item")
-        
-        let listingTitle = titleTextField.text
-        let startPrice = Int(startingPriceTextField.text!)
-        let endDate = endDateTextField.text
-        let desc = descTextArea.text
-        
-        let listingDetails:NSMutableDictionary = [
-            "title": listingTitle ?? "Test",
-            "startPrice": startPrice ?? -1,
-            "endDate": endDate ?? "endDate",
-            "desc": desc ?? "desc",
-            "endDate": " ",
-            "seller":" ",
-            "buyoutPrice": " ",
-            "currentPrice": startPrice ?? -1
-        ]
-        
-        let dbreference = ref.child("listings").childByAutoId()
-        
-        print(dbreference)
-        print(dbreference.description() as String)
-        
-        let image = (addPhotosImage.image)!
-        let imageData = UIImageJPEGRepresentation(image, 0.8)
-        
-        uploadImageToFirebase(data: imageData!, listingData: listingDetails, dbreference: dbreference)
-    }
-    
-    func uploadImageToFirebase(data: Data, listingData: NSMutableDictionary, dbreference: FIRDatabaseReference) {
-        let dbrefString = String(dbreference.description().characters.suffix(20))
-        
-        let storageRef = FIRStorage.storage().reference(withPath: "listingImages/\(dbrefString).jpg")
-        let uploadMetadata = FIRStorageMetadata()
-        uploadMetadata.contentType = "image/jpeg"
-        
-        var downloadURL:String!
-        print(storageRef)
-        
-        storageRef.put(data as Data, metadata: uploadMetadata) { (metadata, error) in
-            if (error != nil) {
-                // Uh-oh, an error occurred!
-            } else {
-                // Metadata contains file metadata such as size, content-type, and download URL.
-                downloadURL = (metadata!.downloadURL()?.absoluteString)!
-                print("download URL 1 \(downloadURL)")
-                
-                listingData.addEntries(from: ["imageURL": [downloadURL]])
-                
-                print(listingData)
-                self.uploadListingToDB(listingData, dbreference: dbreference)
-            }
-        }
-    }
-    
-    func uploadListingToDB(_ listingDetails: NSMutableDictionary, dbreference: FIRDatabaseReference) {
-        print(listingDetails)
-        
-        dbreference.setValue(listingDetails) { (error, ref) -> Void in
-            if (error != nil) {
-                print("ERROR")
-            } else {
-                print("Success")
-                
-                self.titleTextField.text = ""
-                self.startingPriceTextField.text = ""
-                self.endDateTextField.text = ""
-                self.descTextArea.text = ""
-                self.addPhotosImage.image = UIImage(named: "addPhotoImage")
-            }
-        }
-    }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+}
+
+extension CameraViewController: UITextViewDelegate {
 }
