39655ec1c462f384ee6b0243d791f86c954d6ce4
Jan Clarin <hi@janclarin.com>
2016-11-21 11:07:43 -0700

    Remove unused template code

--- Lulu/LoginViewController.swift
+++ Lulu/LoginViewController.swift
@@ -83,16 +83,6 @@ class LoginViewController: UIViewController {
             self.ref.child("users").child(user.uid).setValue(userDict)
         })
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 // MARK: - FBSDKLoginButtonDelegate protocol
