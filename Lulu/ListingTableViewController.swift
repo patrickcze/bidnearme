8ed06d95336fbdcccce8a0bb2dd9d4d7710b4ad9
Jan Clarin <hi@janclarin.com>
2016-11-23 01:10:58 -0700

    Remove dummy code and replace with base code for listings

--- Lulu/ListingTableViewController.swift
+++ Lulu/ListingTableViewController.swift
@@ -7,74 +7,60 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ListingTableViewController: UITableViewController {
     
     // MARK: - Properties
-    var listings : [Listing]!
     let cellIdentifier = "ProfileCell"
-    var listingType = -1
+    var listingsRef: FIRDatabaseReference!
+    var listingIds: [String]!
+    var listings: [Listing]!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        listingsRef = FIRDatabase.database().reference().child("listings")
+        retrieveListings()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // registering the tableViewCell I made, so it can be used
+        // Register custom TableViewCell nib.
         let nib = UINib(nibName: "ProfileTableViewCell", bundle: nil)
         self.tableView.register(ProfileTableViewCell.self, forCellReuseIdentifier: cellIdentifier)
         self.tableView.register(nib,forCellReuseIdentifier: cellIdentifier)
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func retrieveListings() {
+        listings = []
+        // TODO:
+        /*
+        for listingId in listingIds {
+
+            listingsRef.child(listingId).observeSingleEvent(of: .value, with: { (snapshot)
+                let listing = snapshot.value as? [String: Any]
+                self.listings.append()
+            })
+        }
+        */
     }
     
     // MARK: - Table view data source
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return listings.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! ProfileTableViewCell
         let index = indexPath as NSIndexPath
         let listing = listings[index.row]
         cell.itemTitle.text = listing.title
-        cell.itemPhoto.image = UIImage(named: "duckCar")
-        
-        switch listingType {
-        case 0, 4:
-            cell.smallLabel.text = " bidders"
-        case 1,3:
-            cell.smallLabel.text = " date"
-        case 2:
-            cell.smallLabel.text = " Highest bid"
-        default:
-            print("Default -> ListingTableViewController -> cellForRowAt (Profile)")
-        }
+        cell.itemPhoto.image = UIImage()
         return cell
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
 }
