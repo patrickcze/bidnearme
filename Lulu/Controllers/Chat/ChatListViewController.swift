334beed406ae686fb9751750ca46734c636a070f
Jan Clarin <hi@janclarin.com>
2016-12-05 14:24:32 -0700

    Allow ChatListViewController to segue to ChatMessagesViewController

--- Lulu/Controllers/Chat/ChatListViewController.swift
+++ Lulu/Controllers/Chat/ChatListViewController.swift
@@ -13,6 +13,7 @@ final class ChatListViewController: UITableViewController {
     
     // MARK: - Properties
     let cellIdentifier = "ChatCell"
+    var loggedInUser: User?
     var chats: [Chat] = []
     
     override func viewDidLoad() {
@@ -26,6 +27,7 @@ final class ChatListViewController: UITableViewController {
         
         // Get every chat from the user and display them.
         getUserById(userId: loggedInUserId) { (user) in
+            self.loggedInUser = user
             for chatId in user.chats {
                 getChatById(chatId) { (chat) in
                     if let chat = chat {
@@ -38,7 +40,7 @@ final class ChatListViewController: UITableViewController {
 
     }
     
-    // MARK: UITTableViewDataSource
+    // MARK: - UITTableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return chats.count
     }
@@ -51,5 +53,32 @@ final class ChatListViewController: UITableViewController {
         cell.textLabel?.text = chat.listingUid
         
         return cell
+    }
+    
+    /**
+     Segue to ChatMessagesViewController when a chat is selected.
+     */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedChat = chats[indexPath.row]
+        self.performSegue(withIdentifier: "ShowChatMessages", sender: selectedChat)
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        if segue.identifier == "ShowChatMessages" {
+            guard let loggedInUser = loggedInUser else {
+                // Cannot show chats if the user has not been retrieved.
+                return
+            }
+            
+            if let chat = sender as? Chat {
+                let chatMessagesViewController = segue.destination as! ChatMessagesViewController
+                chatMessagesViewController.senderId = loggedInUser.uid
+                chatMessagesViewController.senderDisplayName = loggedInUser.name
+                chatMessagesViewController.chat = chat
+            }
+        }
     }
 }
