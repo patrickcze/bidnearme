79d7298292dd4bb79351a099c3fafe2b3cd3748f
Jan Clarin <hi@janclarin.com>
2016-12-05 00:50:07 -0700

    Display user chats in ChatListViewController

--- Lulu/Controllers/Chat/ChatListViewController.swift
+++ Lulu/Controllers/Chat/ChatListViewController.swift
@@ -7,35 +7,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 final class ChatListViewController: UITableViewController {
     
     // MARK: - Properties
     let cellIdentifier = "ChatCell"
-    var chatList: [Chat] = []
-    var loggedInUser: User = User()
+    var chats: [Chat] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getChatById(chatId: "") { (chat) in
-            self.chatList.append(chat)
+        guard let loggedInUserId = FIRAuth.auth()?.currentUser?.uid else {
+            // Not logged in, so no user chats. Don't load anything.
+            // TODO: Prompt log in.
+            return
         }
         
-        self.tableView.reloadData()
+        // Get every chat from the user and display them.
+        getUserById(userId: loggedInUserId) { (user) in
+            for chatId in user.chats {
+                getChatById(chatId) { (chat) in
+                    if let chat = chat {
+                        self.chats.append(chat)
+                    }
+                }
+                self.tableView.reloadData()
+            }
+        }
+
     }
     
     // MARK: UITTableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chatList.count
+        return chats.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as UITableViewCell
         
-        let chat = chatList[indexPath.row]
+        let chat = chats[indexPath.row]
         
-        cell.textLabel?.text = "HELLO" //chat.listingUid
+        cell.textLabel?.text = chat.listingUid
         
         return cell
     }
